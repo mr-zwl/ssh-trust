5182463976bb1b3049bfffb51bd5abe16c85e624
mr-zwl <70080444+mr-zwl@users.noreply.github.com>
2023-11-09 13:39:28 +0800

    Update ip_ssh_fenfa.sh

--- ip_ssh_fenfa.sh
+++ ip_ssh_fenfa.sh
@@ -18,11 +18,11 @@ if type sshpass >/dev/null 2>&1; then
 else 
 	echo '正在安装sshpass' 
 	yum  install gcc make -y  >& /dev/null
-    tar -xvf ./sshpass.tar.gz  >& /dev/null
-    cd ./sshpass-1.10  >& /dev/null
-    ./configure >& /dev/null
-    make  >& /dev/null
-    make install >& /dev/null
+    	tar -xvf ./sshpass.tar.gz  >& /dev/null
+    	cd ./sshpass-1.10  >& /dev/null
+    	./configure >& /dev/null
+    	make  >& /dev/null
+    	make install >& /dev/null
 fi
 
 for ip in $ip_list
